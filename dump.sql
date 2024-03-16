3431e7f3970fa68a34d7f7250d6899a67079d2da
AntoMars14 <antonio.marseglia@studenti.unicam.it>
2024-03-16 16:42:00 +0100

    Database dump

---
+++ dump.sql
@@ -0,0 +1,225 @@
+﻿USE [master]
+GO
+
+/****** Object:  Database [ProgettoParadigmi]    Script Date: 16/03/2024 16:33:06 ******/
+CREATE DATABASE [ProgettoParadigmi]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'ProgettoParadigmi', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\ProgettoParadigmi.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'ProgettoParadigmi_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\ProgettoParadigmi_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [ProgettoParadigmi].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET QUERY_STORE = ON
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+
+ALTER DATABASE [ProgettoParadigmi] SET  READ_WRITE 
+GO
+
+
+USE [ProgettoParadigmi]
+GO
+
+/****** Object:  Table [dbo].[Utenti]    Script Date: 16/03/2024 16:37:55 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Utenti](
+	[IdUtente] [int] IDENTITY(1,1) NOT NULL,
+	[EmailUtente] [varchar](30) NOT NULL,
+	[Nome] [varchar](20) NOT NULL,
+	[Cognome] [varchar](20) NOT NULL,
+	[Password] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Utenti] PRIMARY KEY CLUSTERED 
+(
+	[IdUtente] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+USE [ProgettoParadigmi]
+GO
+
+/****** Object:  Table [dbo].[Emails]    Script Date: 16/03/2024 16:38:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Emails](
+	[IdEmail] [int] IDENTITY(1,1) NOT NULL,
+	[Destinatario] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Emails] PRIMARY KEY CLUSTERED 
+(
+	[IdEmail] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+USE [ProgettoParadigmi]
+GO
+
+/****** Object:  Table [dbo].[ListeDistribuzione]    Script Date: 16/03/2024 16:39:19 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ListeDistribuzione](
+	[IdListaDistribuzione] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](30) NOT NULL,
+	[IdProprietario] [int] NOT NULL,
+ CONSTRAINT [PK_ListeDistribuzione] PRIMARY KEY CLUSTERED 
+(
+	[IdListaDistribuzione] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[ListeDistribuzione]  WITH CHECK ADD  CONSTRAINT [FK_ListeDistribuzione_Utenti] FOREIGN KEY([IdProprietario])
+REFERENCES [dbo].[Utenti] ([IdUtente])
+GO
+
+ALTER TABLE [dbo].[ListeDistribuzione] CHECK CONSTRAINT [FK_ListeDistribuzione_Utenti]
+GO
+
+USE [ProgettoParadigmi]
+GO
+
+/****** Object:  Table [dbo].[ListaDistribuzioneEmail]    Script Date: 16/03/2024 16:39:43 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ListaDistribuzioneEmail](
+	[IdListaDistribuzione] [int] NOT NULL,
+	[IdEmail] [int] NOT NULL,
+ CONSTRAINT [PK_ListaDistribuzioneEmail] PRIMARY KEY CLUSTERED 
+(
+	[IdListaDistribuzione] ASC,
+	[IdEmail] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[ListaDistribuzioneEmail]  WITH CHECK ADD  CONSTRAINT [FK_ListaDistribuzioneEmail_Emails] FOREIGN KEY([IdEmail])
+REFERENCES [dbo].[Emails] ([IdEmail])
+GO
+
+ALTER TABLE [dbo].[ListaDistribuzioneEmail] CHECK CONSTRAINT [FK_ListaDistribuzioneEmail_Emails]
+GO
+
+ALTER TABLE [dbo].[ListaDistribuzioneEmail]  WITH CHECK ADD  CONSTRAINT [FK_ListaDistribuzioneEmail_ListeDistribuzione] FOREIGN KEY([IdListaDistribuzione])
+REFERENCES [dbo].[ListeDistribuzione] ([IdListaDistribuzione])
+GO
+
+ALTER TABLE [dbo].[ListaDistribuzioneEmail] CHECK CONSTRAINT [FK_ListaDistribuzioneEmail_ListeDistribuzione]
+GO
+
